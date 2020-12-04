a61df0947d048058be01701b03ef24d71b835118
Elsa Yang <elsajy@berkeley.edu>
2020-12-04 01:28:32 -0800

    Removed custom HTML elements for pure HTML code injection in js

--- js/site-header.js
+++ js/site-header.js
@@ -1,7 +1,7 @@
 // site-header adds scripts: js/`hamburger`.js
 
-const headerTemplate = document.createElement('template');
-headerTemplate.innerHTML = `
+// 1. Insert header as HTML text
+var header = `
 <header id="site-header">
     <div class="container-page padding-1-5em">
         <nav class="nav-bar flex-row">
@@ -23,20 +23,9 @@ headerTemplate.innerHTML = `
     </div>
 </header>
 `;
+var main = document.getElementsByTagName('main');
+main[0].insertAdjacentHTML("beforebegin", header);
 
-class SiteHeader extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    // Runs when custom element is added to DOM
-    connectedCallback() {
-        console.log("Successfully rendered custom site-header element. See js/example.js for info.");
-        this.innerHTML = headerTemplate.innerHTML;
-    }
-}
-
-customElements.define("site-header", SiteHeader);
 addScript('text/javascript', 'js/hamburger.js', true);
 
 /* Helper Functions */
@@ -47,3 +36,42 @@ function addScript(type, src, defer) {
     script.defer = defer;
     document.body.appendChild(script);
 }
+
+// 2. (UNUSED) Create a custom site-header element
+// const headerTemplate = document.createElement('template');
+// headerTemplate.innerHTML = `
+// <header id="site-header">
+//     <div class="container-page padding-1-5em">
+//         <nav class="nav-bar flex-row">
+//             <a class="logo" href="./">elsajyang:/ $</a>
+//             <button class="hamburger hamburger--stack" type="button">
+//                 <div class="inner">
+//                     <span class="bar"></span>
+//                     <span class="bar"></span>
+//                     <span class="bar"></span>
+//                 </div>
+//             </button>
+//             <div id="site-nav" class="toggle-nav-menu nav-menu flex-row">
+//                 <a class="logo" href="./">elsajyang:/ $</a>
+//                 <a href="about.html">/about</a>
+//                 <a href="projects.html">/projects</a>
+//                 <a href="posts.html">/posts</a>
+//             </div>
+//         </nav>
+//     </div>
+// </header>
+// `;
+
+// class SiteHeader extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
+
+//     // Runs when custom element is added to DOM
+//     connectedCallback() {
+//         console.log("Successfully rendered custom site-header element. See js/example.js for info.");
+//         this.innerHTML = headerTemplate.innerHTML;
+//     }
+// }
+
+// customElements.define("site-header", SiteHeader);
